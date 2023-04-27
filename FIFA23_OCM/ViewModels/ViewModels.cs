55c4e85c1f6613399b0dcdca4b8fb798e0dfbd93
Kevin Villatoro <kevin.villatoro19@gmail.com>
2023-04-27 18:47:41 -0500

    whoopsie

--- FIFA23_OCM/ViewModels/ViewModels.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace FIFA23_OCM.ViewModels
-{
-    public class TeamBudgetViewModel
-    {
-        public Dictionary<string, decimal> TeamBudgets { get; set; }
-    }
-}
